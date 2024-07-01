ffebfcd3dec2319e808c451977bf766917f69368
Rayhan Pangestu Wibowo <rayhanwibowo24@gmail.com>
2024-07-01 16:55:15 +0700

    Add Layout For User

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Beranda']);
+});
+
+Route::get('/articles', function () {
+    return view('articles', ['title' => 'Artikel']);
+});
+
+Route::get('/report', function () {
+    return view('report', ['title' => 'Laporan']);
+});
+
+Route::get('/educations', function () {
+    return view('educations', ['title' => 'Edukasi']);
 });
